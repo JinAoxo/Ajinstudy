7ed0cbab5708865273e6632433f2d355f0e5327c
조아진 <jinaj777@inseq.co.kr>
2022-08-26 13:59:24 +0900

    개인 스터디 plan 1차 커밋(layer)

---
+++ js/project/D____.js
@@ -0,0 +1,9 @@
+// D project issue
+// Review
+// 1. 해당 프로젝트의 경우 가장 큰 이슈 js, jq 로 작업하는 부분이 많음 (예외 , 공통 多)
+
+/** tooltip 인터렉션 :  주석 처리에 관해서 공부 필요
+ * @param {string} btnOpen
+ * @param {string} btnClose
+ * @returns
+ */
