49d26eb1b426421465c2ade43d8664723e6d1be4
조아진 <jinaj777@inseq.co.kr>
2022-11-29 17:25:01 +0900

    중간 커밋

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  let [data, b] = useState(['data1', 'data2', 'data3'])
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div>{ data[0] }</div>
+      <div>{ data[1] }</div>
+      <div>{data[2]}</div>
+      
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +28,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
